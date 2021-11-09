0ba524d31842fc4ce1d6ec5b3395736f076b3abd
sunanmau5 <sunan.regi@gmail.com>
2021-11-09 16:00:07 +0100

    Initial Commit

---
+++ roman-calculator/roman_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  roman_calculatorApp.swift
+//  roman-calculator
+//
+//  Created by MBA on 09.11.21.
+//
+
+import SwiftUI
+
+@main
+struct roman_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
